ca986b1d6a5b6c676584c7c80621b4c01816e6ba
Nobuo Hayashi <haya.biz@gmail.com>
2017-05-01 04:12:04 +0900

    showDailyPicts :Sort revers order

--- showDailyPicts.php
+++ showDailyPicts.php
@@ -1,14 +1,14 @@
 <?php echo "<h1>指定日の撮影画像一覧　　<button type='button' onclick='history.back()'>トップページに戻る</button></h1>";
 $reqDate = $_POST['showdate'];
 
-  foreach(glob(rtrim("./daily_timelapse/*".$reqDate."*.jpg")) as $file) { //added poceeding wildcard。表示隼を考えること
+  foreach(glob(rtrim("./daily_timelapse/*".$reqDate."*.jpg")) as $file) { //added poceeding wildcard。表示順を変えること
     $result[] = $file;
 }
 $numberOfPictures = count($result);
 echo "<!DOCTYPE html><html><head><meta charset='utf-8'>";
 echo "</head>";
 echo "<body><p>".substr($reqDate, 0, 4)."年".substr($reqDate, 4, 2)."月".substr($reqDate, 6, 2)."日――の定時撮影画像：".$numberOfPictures."枚ありました<p>";
-for ($i = 0; $i < $numberOfPictures; $i++) {
+for ($i = $numberOfPictures; $i >= 0; $i--) {
     echo "<a href=";
     echo $result[$i];
     echo ">";
