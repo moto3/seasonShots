bef7e99be918d0385da14d94e0e5ba5a8db45fb4
Nobuo Hayashi <haya.biz@gmail.com>
2017-05-02 18:59:20 +0900

    add test program list files 30 hours before

---
+++ testListWithin40h.php
@@ -0,0 +1,19 @@
+<?php
+date_default_timezone_set('Asia/Tokyo');
+ 
+//削除期限
+$expire = strtotime("30 hours ago");
+ 
+//ディレクトリ
+$dir = dirname(__FILE__) . '/daily_timelapse/';
+ 
+$list = scandir($dir);
+foreach($list as $value){
+    $file = $dir . $value;
+    if(!is_file($file)) continue;
+    $mod = filemtime( $file );
+    if($mod < $expire){
+        //chmod($file, 0666);
+        print($file);
+    }
+}
