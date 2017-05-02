191956efd9df008514f975efdde113fe59fe1d8f
Nobuo Hayashi <haya.biz@gmail.com>
2017-05-02 14:54:31 +0900

    added test program for get file information

---
+++ testGetFileCreated.php
@@ -0,0 +1,18 @@
+function get_file_list($dir='./daily_taimelapse') {
+    $temp_file = array();
+    if ($h_dir = opendir($dir)) {
+        while (false !== ($filename = readdir($h_dir))) {
+            if ($filename != '.' && $filename != '..') {
+                if (is_file($dir . '/' . $filename)) {
+                    $temp_file[$filename] = filectime($filename);
+                }
+            }
+        }
+    } else {
+        exit("ERROR : cannot open directory $dir. ");
+    }
+    arsort($temp_file);
+    return $temp_file;
+}
+
+print_r(get_file_list());
