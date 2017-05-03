a1c639788e994c21fb3e25255c43d15fd5939eb7
Nobuo Hayashi <haya.biz@gmail.com>
2017-05-03 13:16:19 +0900

    added line feed at file modified date.

--- testListWithin8h.php
+++ testListWithin8h.php
@@ -18,7 +18,7 @@ foreach($list as $value){
     if($mod > $expire){ //指定時間よりも新しいものを抽出
         //chmod($file, 0666);
         print("\$expire = " . $expire.PHP_EOL);
-        echo "$file was last modified at: " . date ("F d Y H:i:s.", $mod);
+        echo "$file was last modified at: " . date ("F d Y H:i:s.", $mod.PHP_EOL);
         print("\$mod = " . $mod.PHP_EOL);
         print($file.PHP_EOL);
     }
