7badc6016c6bd7da7c49964de9693d46eb6e66cf
Nobuo Hayashi <haya.biz@gmail.com>
2017-05-29 16:40:25 +0900

    get all files list on requested day, getAllFileList.php

---
+++ getAllFileList.php
@@ -0,0 +1,13 @@
+<?php 
+header('Content-type: text/javascript; charset=utf-8');
+
+$imageDirectory = 'daily_timelapse/';
+$reqDate = $_GET['showDate'];
+  foreach(glob(rtrim($imageDirectory."*".$reqDate."*.jpg")) as $file) {
+    $result[] = $file;
+}
+ //json化してデータを返す
+$jsonTest = json_encode($result);
+
+echo "document.getElementById('id_fileList').textContent=".$jsonTest.";";
+ ?>
