b863305fa30995512e36f0ae048688f3cd358578
Nobuo Hayashi <haya.biz@gmail.com>
2017-05-29 18:05:32 +0900

    add sorting on timestamp function to getAllFileList.php

--- getAllFileList.php
+++ getAllFileList.php
@@ -6,6 +6,13 @@ $reqDate = $_GET['showDate'];
   foreach(glob(rtrim($imageDirectory."*".$reqDate."*.jpg")) as $file) {
     $result[] = $file;
 }
+//得られたファイルリストを作成時刻でソーティングし直す
+array_multisort(
+  array_map( 'filemtime', $result ),
+  SORT_NUMERIC,
+  SORT_DESC,  //これで新しい写真から順番に表示
+  $result
+);
  //json化してデータを返す
 $jsonTest = json_encode($result);
 
