047bf514015298234b17e4a7e96c6e7c734c6128
Nobuo Hayashi <haya.biz@gmail.com>
2017-06-17 02:40:56 +0900

    add sandBox capability

---
+++ getAllFileList_sandBox.php
@@ -0,0 +1,20 @@
+<?php 
+header('Content-type: text/javascript; charset=utf-8');
+
+$imageDirectory = 'daily_timelapse/';
+$reqDate = $_GET['showDate'];
+  foreach(glob(rtrim($imageDirectory."*".$reqDate."*.jpg")) as $file) {
+    $result[] = $file;
+}
+//得られたファイルリストを作成時刻でソーティングし直す
+array_multisort(
+  array_map( 'filemtime', $result ),
+  SORT_NUMERIC,
+  SORT_ASC,  //これで古い写真から順番に表示
+  $result
+);
+ //json化してデータを返す
+$jsonTest = json_encode($result);
+
+echo "document.getElementById('id_fileList').textContent=".$jsonTest.";";
+ ?>
---
+++ getFileListHourly_v5_sandBox.php
@@ -0,0 +1,61 @@
+<?php 
+header('Content-type: text/javascript; charset=utf-8');
+
+//$reqHour = '13';
+//$beginDate = '20160822';
+$reqHour = $_GET['showHour'];
+$beginDate = $_GET['beginDate'];
+$result = array();
+$imageDirectory = 'daily_timelapse_sandBox/';
+
+  foreach(glob(rtrim($imageDirectory."*".$reqHour."00.jpg")) as $file) {
+    $result[] = $file;
+}
+
+foreach($result as $value){
+//echo '$result='.$value;
+//echo(PHP_EOL);
+}
+
+
+foreach ($result as $key => $value){ //指定された開始日より前のファイルはオミットする
+
+$datePictureTaken = substr( $value, strlen($imageDirectory),8); 
+//ディレクトリー名を削除して…（./".$imageDirectory）なので。+2とする
+
+//echo "datePictureTaken=".$datePictureTaken;
+//echo(PHP_EOL);
+
+if( $beginDate > $datePictureTaken ){ 
+      unset($result[$key]);
+  }
+}
+
+//Indexを詰める
+$result = array_values($result);
+
+/*
+echo "現在の配列ポインターが差す価＝".current($result); 
+
+echo(PHP_EOL);
+echo "■行列削除後の\$key=".$key;
+echo(PHP_EOL);
+echo "■行列削除後の\$value=".$value;
+echo(PHP_EOL);
+*/
+
+//デバッグのために入れた
+//foreach($result as $value){
+
+//echo(PHP_EOL);
+//echo "\$resultの配列内容=".$value;
+  
+//}
+reset($result); //これは呼ぶ必要があるのか、どうか？
+
+ //json化してデータを返す
+$jsonTest = json_encode($result);
+//echo(PHP_EOL);
+echo "document.getElementById('id_fileList').textContent=".$jsonTest.";";
+//echo(PHP_EOL);
+?>
